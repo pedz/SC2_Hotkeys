d026689d6dacbc568beb7498cc30403db373fee5
Perry Smith <pedz@easesoftware.com>
2018-03-30 08:14:36 -0500

    Cleaning up my desktop and needed to put the old defaults file somewhere

--- bin/diff-keymap.sh
+++ bin/diff-keymap.sh
@@ -4,7 +4,9 @@ if [[ $# != 2 ]] ; then
   echo "Usage: $( basename $0 ) map1 map2" 1>&2
   exit 1
 fi
+i=1
 for map in "$1" "$2" ; do
-  cat "$map" | tr -d '\r' | sed -e 's%Option%Alt%g' | sort -u -o "/tmp/$map"
+  cat "$map" | tr -d '\r' | sed -e 's%Option%Alt%g' | sort -u -o "/tmp/$i"
+  (( i += 1 ))
 done
-diff "/tmp/$1" "/tmp/$2"
+diff "/tmp/1" "/tmp/2"
