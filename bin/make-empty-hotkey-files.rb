0a21cef1fc3e13eb159a0361e784ea5f5f93eec9
Perry Smith <pedz@easesoftware.com>
2018-03-31 18:40:00 -0500

    Changed to use temp.txt

--- bin/make-empty-hotkey-files.rb
+++ bin/make-empty-hotkey-files.rb
@@ -1,25 +1,25 @@
 #!/usr/bin/env ruby
 
-File.open("/Users/pedz/Library/Application Support/Blizzard/StarCraft II/Accounts/71687501/Hotkeys/old/txt/default-key-positions.txt", "r") do |f|
+File.open("/Users/pedz/Library/Application Support/Blizzard/StarCraft II/Accounts/71687501/Hotkeys/old/txt/temp.txt", "r") do |f|
   f.each_line do |line|
     line.chomp!
     md = /\A(?<plus>\+*)(?<rest>[^+]*)\Z/.match(line)
     case md[:plus].length
     when 5
-      @mode = md[:rest].gsub(/[- ()]/, "")
+      @mode = md[:rest].gsub(/[- ()'"]/, "")
       # puts "5 #{@mode}"
       
     when 4
-      @area = md[:rest].gsub(/[- ()]/, "")
+      @area = md[:rest].gsub(/[- ()'"]/, "")
       @units = {}
       # puts "4 #{@area}"
       
     when 3
-      @u_or_s = md[:rest].gsub(/[- ()]/, "")
+      @u_or_s = md[:rest].gsub(/[- ()'"]/, "")
       # puts "3 #{@u_or_s}"
       
     when 2
-      @thing = md[:rest].gsub(/[- ()]/, "")
+      @thing = md[:rest].gsub(/[- ()'"]/, "")
       if @units.has_key?(@thing)
         @units[@thing] += 1
       else
@@ -29,7 +29,7 @@ File.open("/Users/pedz/Library/Application Support/Blizzard/StarCraft II/Account
       # puts "2 #{@thing}"
       
     when 1
-      @card = md[:rest].gsub(/[- ()]/, "")
+      @card = md[:rest].gsub(/[- ()'"]/, "")
       if (cnt = @units[@thing]) == 1
         cnt = ""
       end
